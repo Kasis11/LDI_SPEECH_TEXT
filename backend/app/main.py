51227695d93811094c5d9397ddff25e6fca714d5
Kasis11 <21cp032@bvmengineering.ac.in>
2026-07-03 19:00:14 +0530

    updated main

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,10 +25,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8132",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
